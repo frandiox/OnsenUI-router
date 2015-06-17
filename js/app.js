9b0cf97ea1dcd24533f9da454bbc97f1546bc031
Fran Dios <frankdiox@gmail.com>
2015-06-17 12:11:35 +0900

    Indentation fix

--- js/app.js
+++ js/app.js
@@ -3,59 +3,59 @@ var app = angular.module('myApp', ['onsen', 'ui.router']);
 
 app.config(function($stateProvider, $urlRouterProvider) {
 
-  $urlRouterProvider.otherwise("/master");
+	$urlRouterProvider.otherwise("/master");
 
-  $stateProvider
-    // First page of MasterDetail
-    .state('master', {
-      url: '/master',
-      onEnter: ['$rootScope', function($rootScope) {
-        $rootScope.myTabbar.setActiveTab(0);
-        $rootScope.myTabbar.loadPage('html/tab1.html');
-      }]
-    })
-    // Second page of MasterDetail
-    .state('detail', {
-      parent: 'master',
-      url: '/detail/:index',
-      onEnter: ['$rootScope','$stateParams', function($rootScope,$stateParams) {
-        $rootScope.myNavigator.pushPage('html/detail.html', {'index': $stateParams.index});
-      }],
-      onExit: function($rootScope) {
-        $rootScope.myNavigator.popPage();
-      }
-    })
-    .state('sliding', {
-      url: '/sliding',
-      onEnter: ['$rootScope', '$state', '$timeout', function($rootScope, $state, $timeout) {
-          $rootScope.myTabbar.setActiveTab(1);
-          $rootScope.myTabbar.loadPage('html/tab2.html');
-          $timeout(function(){
-            $state.go('main');
-          });
-      }]
-    })
-    .state('main', {
-      parent: 'sliding',
-      url: '/main',
-      onEnter: ['$rootScope', function($rootScope) {
-        $rootScope.myMenu.setMainPage('html/main.html', {closeMenu: true});
-      }]
-    })
-    .state('page1', {
-      parent: 'sliding',
-      url: '/page1',
-      onEnter: ['$rootScope', function($rootScope) {
-        $rootScope.myMenu.setMainPage('html/page1.html', {closeMenu: true});
-      }]
-    })
-    .state('page2', {
-      parent: 'sliding',
-      url: '/page2',
-      onEnter: ['$rootScope', function($rootScope) {
-        $rootScope.myMenu.setMainPage('html/page2.html', {closeMenu: true});
-      }]
-    })
-  ;
+	$stateProvider
+		// First page of MasterDetail
+		.state('master', {
+			url: '/master',
+			onEnter: ['$rootScope', function($rootScope) {
+				$rootScope.myTabbar.setActiveTab(0);
+				$rootScope.myTabbar.loadPage('html/tab1.html');
+			}]
+		})
+		// Second page of MasterDetail
+		.state('detail', {
+			parent: 'master',
+			url: '/detail/:index',
+			onEnter: ['$rootScope','$stateParams', function($rootScope,$stateParams) {
+				$rootScope.myNavigator.pushPage('html/detail.html', {'index': $stateParams.index});
+			}],
+			onExit: function($rootScope) {
+				$rootScope.myNavigator.popPage();
+			}
+		})
+		.state('sliding', {
+			url: '/sliding',
+			onEnter: ['$rootScope', '$state', '$timeout', function($rootScope, $state, $timeout) {
+					$rootScope.myTabbar.setActiveTab(1);
+					$rootScope.myTabbar.loadPage('html/tab2.html');
+					$timeout(function(){
+						$state.go('main');
+					});
+			}]
+		})
+		.state('main', {
+			parent: 'sliding',
+			url: '/main',
+			onEnter: ['$rootScope', function($rootScope) {
+				$rootScope.myMenu.setMainPage('html/main.html', {closeMenu: true});
+			}]
+		})
+		.state('page1', {
+			parent: 'sliding',
+			url: '/page1',
+			onEnter: ['$rootScope', function($rootScope) {
+				$rootScope.myMenu.setMainPage('html/page1.html', {closeMenu: true});
+			}]
+		})
+		.state('page2', {
+			parent: 'sliding',
+			url: '/page2',
+			onEnter: ['$rootScope', function($rootScope) {
+				$rootScope.myMenu.setMainPage('html/page2.html', {closeMenu: true});
+			}]
+		})
+	;
 
 });
--- js/controllers.js
+++ js/controllers.js
@@ -2,38 +2,38 @@
 angular.module('myApp')
 
 .controller('MasterController', function($scope, $data) {
-    $scope.items = $data.items;
+	$scope.items = $data.items;
 })
 
 .controller('DetailController', function($scope, $data) {
-    $scope.item = $data.items[$scope.myNavigator.getCurrentPage().options.index];
+	$scope.item = $data.items[$scope.myNavigator.getCurrentPage().options.index];
 })
 
-.factory('$data', function() {
-      var data = {};
+.factory('$data',function() {
+		var data = {};
 
-      data.items = [
-          {
-              title: 'Item Title',
-              label: '4 h',
-              desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-          },
-          {
-              title: 'Another Item Title',
-              label: '6 h',
-              desc: 'Ut enim ad minim veniam.'
-          },
-          {
-              title: 'Yet Another Item Title',
-              label: '1 day ago',
-              desc: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
-          },
-          {
-              title: 'And One More Item Title',
-              label: '3 days ago',
-              desc: 'Minim veniam aute irure dolor in eiusmod tempor incididunt ut labore et dolore eu fugiat nulla pariatur.'
-          }
-      ];
+		data.items = [
+			{
+				title: 'Item Title',
+				label: '4 h',
+				desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+			},
+			{
+				title: 'Another Item Title',
+				label: '6 h',
+				desc: 'Ut enim ad minim veniam.'
+			},
+			{
+				title: 'Yet Another Item Title',
+				label: '1 day ago',
+				desc: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
+			},
+			{
+				title: 'And One More Item Title',
+				label: '3 days ago',
+				desc: 'Minim veniam aute irure dolor in eiusmod tempor incididunt ut labore et dolore eu fugiat nulla pariatur.'
+			}
+		];
 
-      return data;
-  });
+		return data;
+	});
